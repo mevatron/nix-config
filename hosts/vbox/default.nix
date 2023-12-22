028e217c2d7c63c71a585ee5f985a47f3420be61
Will Lucas <william.lucas@telos.com>
2023-12-22 14:22:23 -0600

    Try another option to get grub EFI working again

--- hosts/vbox/default.nix
+++ hosts/vbox/default.nix
@@ -15,15 +15,15 @@
 
   # Bootloader.
   boot.loader = {
-    # efi = {
-    #   canTouchEfiVariables = true;
-    #   efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
-    # };
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
+    };
     grub = {
       enable = true;
       device = "nodev";  #  "nodev"
       efiSupport = true;
-      efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
+      #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
     };
   };
 

341ef498cc23f979c1d5c3b611dae8f0612fcea5
Will Lucas <mevatron@gmail.com>
2023-12-29 21:16:36 -0600

    Added go and dotnet SDKs

--- home/programs/developer-tools.nix
+++ home/programs/developer-tools.nix
@@ -2,9 +2,11 @@
     home.packages = with pkgs-unstable; [
         clang_17
         jetbrains.clion
+        go
         jetbrains.goland
         jetbrains.idea-ultimate
         jetbrains.pycharm-professional
+        dotnet-sdk_8
         jetbrains.rider
         jetbrains.webstorm
     ];

cf289c3c1130e51da6a43692640cb35e0d909eeb
Will Lucas <mevatron@gmail.com>
2024-01-02 20:28:32 -0600

    Updated system to nixos-hardware upstream after PR went in

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixos-hardware.url = "github:mevatron/nixos-hardware/pr-add-lenovo-legion-16aph8";
+    nixos-hardware.url = "github:nixos/nixos-hardware/master";
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };

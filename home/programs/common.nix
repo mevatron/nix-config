bf0e69dc1b193e57231d7ebc360207cce31eb25f
Will Lucas <mevatron@gmail.com>
2024-02-11 15:34:42 -0600

    Integrated vim config into home-manager

--- home/programs/common.nix
+++ home/programs/common.nix
@@ -25,6 +25,45 @@
       extraConfig = "mouse on";
     };
 
+    vim = {
+      enable = true;
+      plugins = with pkgs.vimPlugins; [
+        ctrlp-vim
+        gruvbox
+        nerdtree
+        vim-airline
+        vim-airline-themes
+        vim-fugitive
+        vim-surround
+      ];
+      settings = { ignorecase = true; };
+      extraConfig = ''
+        set mouse=a
+        let g:airline_powerline_fonts = 1
+        set t_Co=256
+        let g:airline_theme='murmur'
+
+        silent! colorscheme gruvbox
+        set background=dark
+
+        let mapleader = ","
+        map <leader>f :CtrlP<CR>
+        map <leader>n :NERDTreeToggle<CR>
+        map <leader>t <Esc>:tabnew<CR>
+
+        inoremap kj <Esc>
+        cnoremap kj <C-C>
+
+        :set number
+        :set relativenumber
+        :set cursorline
+
+        :set tabstop=2
+        :set shiftwidth=2
+        :set expandtab
+      '';
+    };
+
     btop.enable = true; # replacement of htop/nmon
     jq.enable = true; # A lightweight and flexible command-line JSON processor
     ssh.enable = true;

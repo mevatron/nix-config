9984b61099def6d1c7a346093cb320e01884e2e2
Will Lucas <mevatron@gmail.com>
2024-05-04 22:05:52 -0500

    work around FHS env bug

--- home/config/default.nix
+++ home/config/default.nix
@@ -6,6 +6,11 @@
             ".ideavimrc" = {
                 source = config.lib.file.mkOutOfStoreSymlink ./.ideavimrc;
             };
+            # work around home-manager symlink bug inside of FHS environments
+            ".ssh/config" = {
+                target = ".ssh/config_source";
+                onChange = ''cat ~/.ssh/config_source > ~/.ssh/config && chmod 400 ~/.ssh/config'';
+            };
         };
     };
 }

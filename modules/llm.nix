660c33fc8fe71387b993ac2fcc6696a3d7d14935
Will Lucas <mevatron@gmail.com>
2024-08-06 12:23:28 -0500

    Setup open-webui for whenever it gets fixed again

--- modules/llm.nix
+++ modules/llm.nix
@@ -1,17 +1,31 @@
 {config, lib, pkgs-unstable, ...}:
 
 {
-    disabledModules = [ "services/misc/ollama.nix" ];
+    disabledModules = [
+        "services/misc/ollama.nix"
+        #"services/misc/open-webui.nix"
+    ];
 
     imports = [
         (import "${pkgs-unstable.path}/nixos/modules/services/misc/ollama.nix")
+        #(import "${pkgs-unstable.path}/nixos/modules/services/misc/open-webui.nix")
     ];
 
-    services.ollama = {
-        enable = true;
-        acceleration = "cuda";
-        package = pkgs-unstable.ollama;
-        host = "0.0.0.0";
-        port = 11434;
+    services = {
+        ollama = {
+            enable = true;
+            acceleration = "cuda";
+            package = pkgs-unstable.ollama;
+            host = "0.0.0.0";
+            port = 11434;
+        };
+        #open-webui = {
+        #    enable = true;
+        #    package = pkgs-unstable.open-webui;
+        #    host = "0.0.0.0";
+        #    environment = {
+        #        OLLAMA_API_BASE_URL = "http://localhost:11434";
+        #    };
+        #};
     };
 }

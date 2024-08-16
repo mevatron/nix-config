b8c7eabe632a4022a07de2610e86e509969e63c4
Will Lucas <mevatron@gmail.com>
2024-08-16 08:57:35 -0500

    Got open-webui working finally

--- modules/llm.nix
+++ modules/llm.nix
@@ -3,12 +3,12 @@
 {
     disabledModules = [
         "services/misc/ollama.nix"
-        #"services/misc/open-webui.nix"
+        "services/misc/open-webui.nix"
     ];
 
     imports = [
         (import "${pkgs-unstable.path}/nixos/modules/services/misc/ollama.nix")
-        #(import "${pkgs-unstable.path}/nixos/modules/services/misc/open-webui.nix")
+        (import "${pkgs-unstable.path}/nixos/modules/services/misc/open-webui.nix")
     ];
 
     services = {
@@ -19,13 +19,13 @@
             host = "0.0.0.0";
             port = 11434;
         };
-        #open-webui = {
-        #    enable = true;
-        #    package = pkgs-unstable.open-webui;
-        #    host = "0.0.0.0";
-        #    environment = {
-        #        OLLAMA_API_BASE_URL = "http://localhost:11434";
-        #    };
-        #};
+        open-webui = {
+            enable = true;
+            package = pkgs-unstable.open-webui;
+            host = "0.0.0.0";
+            environment = {
+                OLLAMA_API_BASE_URL = "http://localhost:11434";
+            };
+        };
     };
 }

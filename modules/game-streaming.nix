6662dd804d6fd76a2a56b3ce0c67dcf0c608b9ed
Will Lucas <mevatron@gmail.com>
2024-10-21 20:55:40 -0500

    Build sunshine with CUDA support

--- modules/game-streaming.nix
+++ modules/game-streaming.nix
@@ -1,12 +1,25 @@
-{config, pkgs, ...}:
+{ config, pkgs, lib, ...}:
+
+let
+  sunshineOverride = pkgs.sunshine.override {
+    cudaSupport = true;
+    stdenv = pkgs.cudaPackages.backendStdenv;
+  };
+in
+
+with lib;
 
 {
-    services = {
-      sunshine = {
-        enable = true;
-        autoStart = true;
-        capSysAdmin = true;
-        openFirewall = true;
-      };
-    };
+
+  environment.systemPackages = with pkgs; [
+    sunshineOverride
+  ];
+
+  services.sunshine = {
+    package = sunshineOverride;
+    enable = true;
+    autoStart = true;
+    capSysAdmin = true;
+    openFirewall = true;
+  };
 }

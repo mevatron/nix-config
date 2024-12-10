26b4d7642c8488dfc1eb7964017ee28e96a79725
Will Lucas <mevatron@gmail.com>
2024-12-09 22:07:16 -0600

    Update Cura to version 5.9.0 and fix package installation
    This change updates the Cura application from version 5.4.0 to 5.9.0, which includes bug fixes and improvements. The Nix configuration for Cura has also
    been updated to use the new version of the application image, ensuring that users are getting the latest stable release. Additionally, the package
    installation process has been corrected to avoid broken dependencies and ensure a successful installation.

---
+++ overlays/cura/default.nix
@@ -0,0 +1,28 @@
+# overlays/cura/default.nix
+args:
+final: prev: {
+  cura = let
+    cura5 = final.appimageTools.wrapType2 rec {
+      name = "cura5";
+      version = "5.9.0";
+      src = final.fetchurl {
+        url = "https://github.com/Ultimaker/Cura/releases/download/${version}/UltiMaker-Cura-${version}-linux-X64.AppImage";
+        hash = "sha256-STtVeM4Zs+PVSRO3cI0LxnjRDhOxSlttZF+2RIXnAp4=";
+      };
+      extraPkgs = pkgs: with pkgs; [ ];
+    };
+  in final.writeScriptBin "cura" ''
+    #! ${final.bash}/bin/bash
+    # AppImage version of Cura loses current working directory and treats all paths relateive to $HOME.
+    # So we convert each of the files passed as argument to an absolute path.
+    # This fixes use cases like `cd /path/to/my/files; cura mymodel.stl anothermodel.stl`.
+    args=()
+    for a in "$@"; do
+      if [ -e "$a" ]; then
+        a="$(realpath "$a")"
+      fi
+      args+=("$a")
+    done
+    exec "${cura5}/bin/cura5" "''${args[@]}"
+  '';
+}

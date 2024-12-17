f30efa5856937a395b675bfdc9521b2aed0212cb
Will Lucas <mevatron@gmail.com>
2024-12-17 17:39:44 -0600

    Update flake.lock and modify kde.nix configuration
    This commit updates the `flake.lock` file to reflect the latest changes in the Nix packages and repositories. The `flake.lock` file now contains updated
    revision numbers for `nixos-hardware`, `nixpkgs`, `nixpkgs-unstable`, and `kde`. Additionally, the `kde.nix` configuration has been modified to enable more
    services and adjust the timeout settings for `plasma-powerdevil`. This ensures that the system uses the latest dependencies and configurations while
    maintaining compatibility and functionality.

--- modules/kde.nix
+++ modules/kde.nix
@@ -1,16 +1,20 @@
 {config, pkgs, ...}:
 
 {
-    services = {
-        xserver = {
-            enable = true;
-            desktopManager.plasma5.enable = true;
-        };
-
-        displayManager = {
-            sddm = {
-                enable = true;
-            };
-        };
+  services = {
+    xserver = {
+      enable = true;
+      desktopManager.plasma5.enable = true;
     };
+
+    displayManager = {
+      sddm = {
+        enable = true;
+      };
+    };
+  };
+
+  systemd.services.plasma-powerdevil = {
+    serviceConfig.TimeoutSec = "15s";
+  };
 }

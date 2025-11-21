463fe0478638b994d17a375b7dbdeeafc1303b29
Will Lucas <mevatron@gmail.com>
2025-11-20 19:33:23 -0600

    Add `kdenlive` to home packages and configure GitLab SSH settings

--- home/config/default.nix
+++ home/config/default.nix
@@ -12,6 +12,12 @@
             # work around home-manager symlink bug inside of FHS environments
             ".ssh/config" = {
                 target = ".ssh/config_source";
+                text = ''
+                  Host gitlab.com
+                    IdentityFile ~/.ssh/id_gitlab
+                    ForwardAgent no
+                    AddKeysToAgent yes
+                '';
                 onChange = ''cat ~/.ssh/config_source > ~/.ssh/config && chmod 400 ~/.ssh/config'';
             };
         };
--- home/programs/art.nix
+++ home/programs/art.nix
@@ -4,6 +4,7 @@
     home.packages = with pkgs; [
         gimp-with-plugins
         inkscape-with-extensions
+        kdePackages.kdenlive
         krita
         photocollage
     ];

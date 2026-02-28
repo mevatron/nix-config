0307ddacc2fccade273bd733de5603253d40e97e
Will Lucas <mevatron@gmail.com>
2026-02-27 19:57:24 -0600

    Add `qwen-3.5-27b-unsloth` configuration to LLM module

--- modules/llm.nix
+++ modules/llm.nix
@@ -85,6 +85,25 @@
                             --port ''${PORT}
                         '';
                     };
+
+                    "qwen-3.5-27b-unsloth" = {
+                        cmd = ''
+                          ${llama-server} \
+                            -hf unsloth/Qwen3.5-27B-GGUF:UD-Q4_K_XL \
+                            --jinja \
+                            -ngl 99 \
+                            --cache-type-k q8_0 \
+                            --cache-type-v q8_0 \
+                            --ctx-size 65535 \
+                            --temp 0.6 \
+                            --top-p 0.95 \
+                            --top-k 20 \
+                            --min-p 0 \
+                            --chat-template-kwargs "{\"enable_thinking\": true}" \
+                            --host 127.0.0.1 \
+                            --port ''${PORT}
+                        '';
+                    };
                 };
             };
         };

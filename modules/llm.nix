5af7d55f754a36847b5cded8aa3e8ed2e8d9b308
Will Lucas <mevatron@gmail.com>
2026-04-02 12:48:43 -0500

    Update `flake.lock` dependencies and remove `ollama` service from LLM module configuration

--- modules/llm.nix
+++ modules/llm.nix
@@ -2,36 +2,22 @@
 
 {
     disabledModules = [
-        "services/misc/ollama.nix"
         "services/misc/open-webui.nix"
         "services/networking/llama-swap.nix"
     ];
 
     imports = [
-        (import "${pkgs-master.path}/nixos/modules/services/misc/ollama.nix")
         (import "${pkgs-master.path}/nixos/modules/services/misc/open-webui.nix")
         (import "${pkgs-master.path}/nixos/modules/services/networking/llama-swap.nix")
     ];
 
     services = {
-        ollama = {
-            enable = true;
-            package = pkgs-unstable.ollama-cuda;
-            host = "0.0.0.0";
-            port = 11434;
-            environmentVariables = {
-                OLLAMA_DEBUG = "2";
-                OLLAMA_FLASH_ATTENTION = "1";
-                OLLAMA_KV_CACHE_TYPE = "q8_0";
-            };
-        };
         open-webui = {
             enable = true;
             package = pkgs-unstable.open-webui;
             host = "0.0.0.0";
             environment = {
                 HOME = "/var/lib/open-webui";
-                OLLAMA_API_BASE_URL = "http://localhost:11434";
                 WEBUI_AUTH = "False";
                 PYDANTIC_SKIP_VALIDATING_CORE_SCHEMAS = "True";
             };
